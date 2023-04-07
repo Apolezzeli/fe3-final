f5cdacf2775fd642d4d60f69c6f5efc063444b9b
apolezzeli <almendra.polezzeli@flexibility.com.ar>
2023-04-07 20:06:53 -0300

    add navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
-
+import React from 'react';
+import { useContextGlobal } from "./utils/global.context";
+import { Link } from "react-router-dom";
+import { links } from "./utils/links";
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
+import ButtonTheme from "./ButtonTheme/ButtonTheme";
+
 
 const Navbar = () => {
+  const { theme, dispatchTheme } = useContextGlobal();
+  const { home, contact, favs, dentist } = links;
+
+  const handleTheme = () => {
+    console.log(theme);
+    dispatchTheme({ type: theme.color === "light" ? "SET_DARK" : "SET_LIGHT" });
+  };
 
   return (
     <nav>
-      {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
-      {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
-      <button>Change theme</button>
+      <h2>DH Odonto</h2>
+      <div>
+        {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
+        <Link to={home.path}>{home.name}</Link>
+        <Link to={contact.path}>{contact.name}</Link>
+        <Link to={favs.path}>{favs.name}</Link>
+        <Link to={dentist.path}>{dentist.name}</Link>
+        {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
+        <ButtonTheme onClick={handleTheme} />
+      </div>
     </nav>
   )
 }
